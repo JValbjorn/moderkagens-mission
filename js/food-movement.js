dce138c270c518b4dbf4c5ee9179df42edfe6f05
runedrummer81 <runedrummer81@gmail.com>
2025-05-19 17:26:54 +0200

    game1-html animation works 1.4

--- js/food-movement.js
+++ js/food-movement.js
@@ -2,32 +2,45 @@
 
 const madIkoner = [
     { src: "media/img/food-items/blueberry.png", type: "god" },
-    { src: "media/img/food-items/blue-donut.png", type: "dårlig" }
-  ];
+    { src: "media/img/food-items/banana.png", type: "god" },
+    { src: "media/img/food-items/avcado.png", type: "god" },
+    { src: "media/img/food-items/carrot.png", type: "god" },
+    { src: "media/img/food-items/fried-egg.png", type: "god" },
+    { src: "media/img/food-items/salmon.png", type: "god" },
+    { src: "media/img/food-items/strawberry.png", type: "god" },
+    { src: "media/img/food-items/water.png", type: "god" },
+    { src: "media/img/food-items/blue-donut.png", type: "dårlig" },
+    { src: "media/img/food-items/wine.png", type: "dårlig" },
+    { src: "media/img/food-items/fries.png", type: "dårlig" },
+    { src: "media/img/food-items/pizza.png", type: "dårlig" },
+    { src: "media/img/food-items/pink-donut.png", type: "dårlig" },
+    { src: "media/img/food-items/lakrids.png", type: "dårlig" },
+    { src: "media/img/food-items/ginger.png", type: "dårlig" },
+    { src: "media/img/food-items/coffee.png", type: "dårlig" },
+];
 
-  function genererMadikon() {
+function genererMadikon() {
     const ikonData = madIkoner[Math.floor(Math.random() * madIkoner.length)];
     const img = document.createElement("img");
     img.src = ikonData.src;
     img.classList.add("food-icon");
     img.dataset.type = ikonData.type;
-  
-    // Klik-funktion
-    img.addEventListener("click", () => {
-      if (ikonData.type === "god") {
-        console.log("God ernæring!");
-        // Tilføj point, feedback etc.
-      } else {
-        console.log("Dårlig ernæring!");
-      }
-      img.remove(); // fjerner efter klik
-    });
-  
-    document.querySelector(".blodbane-left").appendChild(img);
-  
-    // Fjern ikon efter animation
-    setTimeout(() => img.remove(), 5000);
-  }
 
-  // Generér nyt ikon hver 2. sekund
-setInterval(genererMadikon, 2000); 
+    // Tilføj animationend listener i stedet for setTimeout
+    img.addEventListener("animationend", () => {
+        img.remove();
+    });
+
+    img.addEventListener("click", () => {
+        img.remove();
+        // Tilføj din scoringslogik her senere
+    });
+
+    document.querySelector(".blodbane-left").appendChild(img);
+}
+
+// Justeret til 3 sekunder (3000ms)
+const spawnInterval = setInterval(genererMadikon, 1000);
+
+
+
