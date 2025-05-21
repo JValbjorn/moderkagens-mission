0813517ea1bc8a648dc7d0a6601fca3c175f1f3c
Trier77 <jacob.aa.trier@gmail.com>
2025-05-21 11:12:11 +0200

    added scanner-video.js added new scanner video

---
+++ js/scanner-video.js
@@ -0,0 +1,17 @@
+"use-strict";
+
+    const playBtn = document.getElementById('action-knap');
+    // const scanBtn = document.getElementsByClassName('front-btns')
+    const videoOverlay = document.getElementById('scanner-container');
+    const scannerVideo = document.getElementById('scanner-video');
+    const startScreen = document.getElementById('forside-ui');
+
+    playBtn.addEventListener('click', () => {
+      videoOverlay.style.display = 'flex';
+      startScreen.style.display = 'none';
+      scannerVideo.play();
+    });
+
+    scannerVideo.addEventListener('ended', () => {
+      window.location.href = 'game1-food.html'; // Udskift med din mål-side
+    });
