d3b23c771e69cb245d1bd59e65b789d522bb4e64
Trier77 <jacob.aa.trier@gmail.com>
2025-05-22 14:26:43 +0200

    removed src for font i button

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//object-array
 let scoreArray = [
     { type: "good", quantity: 0, worth: 10, total: 0 },
     { type: "bad", quantity: 0, worth: 5, total: 0 },
@@ -7,22 +8,22 @@ let scoreArray = [
     { type: "virus", quantity: 0, worth: 10, total: 0 }
 ]
 
-// Gemmer kurvens indhold i localStorage
+// Gemmer scoreArray i localStorage
 function savePointsToLocalStorage() {
     localStorage.setItem('pointSystem', JSON.stringify(scoreArray));
 }
 
-// Henter kurvens indhold fra localStorage ved sideindlæsning
+// Henter scoreArray fra localStorage ved sideindlæsning
 function loadPointsFromLocalStorage() {
     const storedPoints = localStorage.getItem('pointSystem');
     if (storedPoints) {
         scoreArray = JSON.parse(storedPoints); // Konverterer string til array
-        updateUIFromCart(); // Opdaterer UI med hentede data
+        updateUIFromCart(); //kalde funktionen updateUIFromCart()
     }
 }
 
 // Opdaterer UI med kurvens data
-//husk at bajer => er en forkortelse af function(bajer), det gør man typisk i f.eks. i en forEach 
+
 function updateUIFromCart() {
     scoreArray.forEach(micro => {
         let antalIndi = document.getElementById(micro.type);
